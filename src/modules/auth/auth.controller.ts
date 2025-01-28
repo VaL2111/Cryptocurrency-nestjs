62caf51700c7f2a2ebae5f0e52f69fbaab9e3733
Valentyn Bahmatskyi <valentunbaxc@gmail.com>
2025-01-28 22:14:44 +0200

    Verify user access token using jwt passport and add access verification to the router using useGuards

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { CreateUserDTO } from "../user/dto";
 import { UserLoginDTO } from "./dto";
 import { AuthUserResponse } from "./response";
 import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "src/guards/jwt-guard";
 
 @Controller("auth")
 export class AuthController {
@@ -22,4 +23,10 @@ export class AuthController {
   login(@Body() dto: UserLoginDTO): Promise<AuthUserResponse> {
     return this.authService.loginUser(dto);
   }
+
+	@UseGuards(JwtAuthGuard)
+	@Post("test")
+	test() {
+		return true;
+	}
 }
